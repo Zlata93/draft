cb46afafa7dd5eda04755cab148940c92ed17e35
Zlata Kotlova <zlata.kotlova@mail.ru>
2019-10-06 11:12:07 +0300

    Syntax highlighter added

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { cn } from '@bem-react/classname';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
+import { codeStyle } from './styles';
 import './Editor.scss';
 
 import IconPlus from '../IconPlus/IconPlus';
 
+SyntaxHighlighter.registerLanguage('javascript', js);
+
 export const cnEditor = cn('Editor');
 
 const Editor = ({ className, headerColor, headerIndentH, headerIndentV, fileName, code, isLoading }) => {
+
     return (
         <div className={cnEditor({}, [className])}>
             <div className={cnEditor('Header',
@@ -24,9 +30,14 @@ const Editor = ({ className, headerColor, headerIndentH, headerIndentV, fileName
                     <div className={cnEditor('Action')}/>
                 </div>
             </div>
-            <pre className={cnEditor('Body')}>
+            <SyntaxHighlighter
+                style={codeStyle}
+                showLineNumbers
+                // lineNumberContainerStyle={{ paddingRight: '15px' }}
+                lineNumberStyle={{ color: '#c7c7c7' }}
+            >
                 {isLoading ? 'Loading...' : (code || 'Could not open the file')}
-            </pre>
+            </SyntaxHighlighter>
         </div>
     );
 };
---
+++ src/components/Editor/styles.js
@@ -0,0 +1,98 @@
+export const codeStyle = {
+    "hljs": {
+        "display": "block",
+        "overflowX": "auto",
+        "padding": "5px 20px",
+        "background": "#fff",
+        "color": "#00234b",
+        "fontSize": "12px",
+        "margin": 0
+    },
+    "hljs-keyword": {
+        "fontWeight": "bold"
+    },
+    "hljs-selector-tag": {
+        "fontWeight": "bold"
+    },
+    "hljs-title": {
+        "fontWeight": "bold",
+        "color": "#005fba"
+    },
+    "hljs-section": {
+        "fontWeight": "bold",
+        "color": "#005fba"
+    },
+    "hljs-doctag": {
+        "fontWeight": "bold"
+    },
+    "hljs-name": {
+        "fontWeight": "bold",
+        "color": "#005fba"
+    },
+    "hljs-strong": {
+        "fontWeight": "bold"
+    },
+    "hljs-comment": {
+        "color": "#8694a2"
+    },
+    "hljs-string": {
+        "color": "#005fba"
+    },
+    "hljs-built_in": {
+        "color": "#005fba"
+    },
+    "hljs-literal": {
+        "color": "#005fba"
+    },
+    "hljs-type": {
+        "color": "#005fba"
+    },
+    "hljs-addition": {
+        "color": "#005fba"
+    },
+    "hljs-tag": {
+        "color": "#005fba"
+    },
+    "hljs-quote": {
+        "color": "#005fba"
+    },
+    "hljs-selector-id": {
+        "color": "#005fba"
+    },
+    "hljs-selector-class": {
+        "color": "#005fba"
+    },
+    "hljs-meta": {
+        "color": "#4c81c9"
+    },
+    "hljs-subst": {
+        "color": "#4c81c9"
+    },
+    "hljs-symbol": {
+        "color": "#4c81c9"
+    },
+    "hljs-regexp": {
+        "color": "#4c81c9"
+    },
+    "hljs-attribute": {
+        "color": "#4c81c9"
+    },
+    "hljs-deletion": {
+        "color": "#4c81c9"
+    },
+    "hljs-variable": {
+        "color": "#4c81c9"
+    },
+    "hljs-template-variable": {
+        "color": "#4c81c9"
+    },
+    "hljs-link": {
+        "color": "#4c81c9"
+    },
+    "hljs-bullet": {
+        "color": "#4c81c9"
+    },
+    "hljs-emphasis": {
+        "fontStyle": "italic"
+    }
+};
