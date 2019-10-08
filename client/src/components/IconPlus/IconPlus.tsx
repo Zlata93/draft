771bb1ffc31734c99c85a1fa57551dddfe1d50c4
Zlata Kotlova <z.kotlova@informika.ru>
2019-10-08 20:44:06 +0300

    IconPlus to ts

--- client/src/components/IconPlus/IconPlus.tsx
+++ client/src/components/IconPlus/IconPlus.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { ReactNode, FC } from 'react';
 import { cn } from '@bem-react/classname';
 import './IconPlus.scss';
 
 export const cnIconPlus = cn('IconPlus');
 
-const IconPlus = ({ type, className, children }) => {
+export interface IconPlusProps {
+    children?: ReactNode | string;
+    className?: string;
+    type: string;
+}
+
+const IconPlus: FC<IconPlusProps> = ({ type, className, children }) => {
     return (
         <div className={cnIconPlus({}, [className])}>
             <div className={cnIconPlus('Icon', { type: type })}/>
