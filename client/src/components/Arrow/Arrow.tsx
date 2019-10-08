8dff0aabf53716c5da315be905ff5deceecbf106
Zlata Kotlova <z.kotlova@informika.ru>
2019-10-08 20:06:08 +0300

    Arrow to ts

---
+++ client/src/components/Arrow/Arrow.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { cn } from '@bem-react/classname';
+import './Arrow.scss';
+
+export const cnArrow = cn('Arrow');
+
+export interface ArrowProps {
+    className?: string;
+}
+
+
+const Arrow: React.FC<ArrowProps> = ({className}) => {
+    return <i className={cnArrow({}, [className])}/>
+};
+
+export default Arrow;
