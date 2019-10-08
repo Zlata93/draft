7f1a4cb41e7f4536b13e6fd5a0276f954156156b
Zlata Kotlova <z.kotlova@informika.ru>
2019-10-08 19:53:01 +0300

    client utils to ts

--- client/src/utils/getPathFromLocation.ts
+++ client/src/utils/getPathFromLocation.ts
@@ -1,4 +1,4 @@
-function getPathFromLocation(locationPathname) {
+function getPathFromLocation(locationPathname: string) {
     if (locationPathname.includes('.')) {
         let pathArr = locationPathname.split('/');
         pathArr = pathArr.slice(0, pathArr.length - 1);
